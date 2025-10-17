5ff052747bdbb7e2bdaff15a5c5490b0f61ed83f
mas-fiqi <moh.rafiqi.wahyudi@mail.com>
2025-10-17 09:08:20 +0700

    awal pembuatan  dan seterutur project

---
+++ lib/data/local_db/hive_manager.dart
@@ -0,0 +1,6 @@
+       // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
